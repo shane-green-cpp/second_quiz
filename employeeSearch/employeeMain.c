d373c44ab403fe395dcf747370a7fe29c5903670
shane green <shanegreen@cpp.edu>
2022-03-10 18:53:15 -0800

    implemented functions, now just need to format

--- employeeSearch/employeeMain.c
+++ employeeSearch/employeeMain.c
@@ -8,26 +8,47 @@ int main(void){
     //defined in employeeSearchOne.c 
     PtrToEmployee searchEmployeeByNumber(const Employee table[], int sizeTable, long numberToFind); 
     PtrToEmployee searchEmployeeByName(const Employee table[], int sizeTable, char * nameToFind); 
+    PtrToEmployee searchEmployeeByPhone(const Employee table[], int sizeTable, char * phoneToFind); 
+    PtrToEmployee searchEmployeeBySalary(const Employee table[], int sizeTable, double salaryToFind); 
 
     //defined in employeeTable.c 
     extern Employee EmployeeTable[];
     extern const int EmployeeTableEntries;
 
     PtrToEmployee matchPtr;  //Declaration 
+    
+    //for number
     matchPtr = searchEmployeeByNumber(EmployeeTable, EmployeeTableEntries, 1045); 
-
     //Example not found 
     if (matchPtr != NULL) 
-        printf("Employee ID 1045 is in record %d\n", matchPtr - EmployeeTable); 
+        printf("Employee ID 1045 is in record %ld\n", matchPtr - EmployeeTable); 
     else 
         printf("Employee ID is NOT found in the record\n"); 
 
+    //for name
     //Example found 
     matchPtr = searchEmployeeByName(EmployeeTable, EmployeeTableEntries, "Tony Bobcat"); 
     if (matchPtr != NULL) 
-        printf("Employee Tony Bobcat is in record %d\n", matchPtr - EmployeeTable); 
+        printf("Employee Tony Bobcat is in record %ld\n", matchPtr - EmployeeTable); 
     else 
         printf("Employee Tony Bobcat is NOT found in the record\n"); 
+
+    //for phone
+    //Example found 
+    matchPtr = searchEmployeeByPhone(EmployeeTable, EmployeeTableEntries, "213-555-1212"); 
+    if (matchPtr != NULL) 
+        printf("Employee phone 213-555-1212 is in record %ld\n", matchPtr - EmployeeTable); 
+    else 
+        printf("Employee phone 213-555-1212 is NOT found in the record\n"); 
+
+    //for phone
+    //Example found 
+    matchPtr = searchEmployeeBySalary(EmployeeTable, EmployeeTableEntries, 6.34); 
+    if (matchPtr != NULL) 
+        printf("Employee salary 6.34 is in record %ld\n", matchPtr - EmployeeTable); 
+    else 
+        printf("Employee salary 6.34 is NOT found in the record\n"); 
+        
 
     return EXIT_SUCCESS; 
 }
--- employeeSearch/employeeOne.c
+++ employeeSearch/employeeOne.c
@@ -6,11 +6,9 @@ PtrToEmployee searchEmployeeByNumber(PtrToConstEmployee ptr, int tableSize, long
     const PtrToConstEmployee endPtr = ptr + tableSize; 
 
     for(; ptr < endPtr; ptr++)  //search until end of table  ptr++ will increment by what?? 
-
     { 
-
-    if(ptr->number == targetNumber)  //check if it equals the Employee number 
-        return (PtrToEmployee) ptr;  //return location of the number to callee. 
+        if(ptr->number == targetNumber)  //check if it equals the Employee number 
+            return (PtrToEmployee) ptr;  //return location of the number to callee. 
     } 
 
     return NULL;  //this will only happen if no Employee number matches in loop above 
@@ -23,12 +21,35 @@ PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int tableSize, char *
     const PtrToConstEmployee endPtr = ptr + tableSize; 
 
     for(; ptr < endPtr; ptr++) 
-
     { 
-
-    if(strcmp(ptr->name,targetName) == 0) 
-        return (PtrToEmployee) ptr; 
+        if(strcmp(ptr->name,targetName) == 0) 
+            return (PtrToEmployee) ptr; 
     } 
 
     return NULL; 
+} 
+
+//Essentially the same functionality as above but comparing strings to check if equal 
+PtrToEmployee searchEmployeeByPhone(PtrToConstEmployee ptr, int tableSize, char * targetPhone) { 
+    const PtrToConstEmployee endPtr = ptr + tableSize; 
+
+    for(; ptr < endPtr; ptr++) 
+    { 
+        if(strcmp(ptr->phone,targetPhone) == 0) 
+            return (PtrToEmployee) ptr; 
+    } 
+
+    return NULL; 
+} 
+
+PtrToEmployee searchEmployeeBySalary(PtrToConstEmployee ptr, int tableSize, double targetSalary){ 
+    const PtrToConstEmployee endPtr = ptr + tableSize; 
+
+    for(; ptr < endPtr; ptr++)  //search until end of table  ptr++ will increment by what?? 
+    { 
+        if(ptr->salary == targetSalary)  //check if it equals the Employee number 
+            return (PtrToEmployee) ptr;  //return location of the number to callee. 
+    } 
+
+    return NULL;  //this will only happen if no Employee number matches in loop above 
 } 
